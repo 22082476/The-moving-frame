bec304f24468b7a97ed3b75ec6eb8162a92fa9a6
22082476 <22082476@student.hhs.nl>
2026-01-31 16:51:20 +0100

    Chore: add config for deploy

--- the-moving-frame/next.config.ts
+++ the-moving-frame/next.config.ts
@@ -1,13 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,       // Recommended for catching bugs
-  experimental: {
-    appDir: true,              // Enable the App Router
-  },
-  images: {
-    unoptimized: true,         // Optional: avoids extra Next.js image optimization on Vercel
-  },
+  reactStrictMode: true
 };
 
 export default nextConfig;
